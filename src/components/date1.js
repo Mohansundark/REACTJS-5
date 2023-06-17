2d6b8d7f39c06c7a2beadb87646eff038293f166
Mohansundark <mohansundar.k2020@vitstudent.ac.in>
2023-06-17 21:07:05 +0530

    first commit

--- src/components/date1.js
+++ src/components/date1.js
@@ -5,36 +5,67 @@ import './date.css';
 const AgeCalculator = () => {
   const [dob, setDob] = useState('');
   const [age, setAge] = useState({ years: 0, months: 0 });
+  const [showAge, setShowAge] = useState(false);
+  const [dateWarning, setDateWarning] = useState(false);
 
   const calculateAge = () => {
     const currentDate = moment();
     const birthDate = moment(dob);
+
+    if (!birthDate.isValid()) {
+      setDateWarning(true);
+      setShowAge(false);
+      setDob(''); // Reset the input field
+      return;
+    }
+
+    if (birthDate.isAfter(currentDate)) {
+      setDateWarning(true);
+      setShowAge(false);
+      setDob(''); // Reset the input field
+      return;
+    }
 
     const yearsDiff = currentDate.diff(birthDate, 'years');
     birthDate.add(yearsDiff, 'years');
     const monthsDiff = currentDate.diff(birthDate, 'months');
 
     setAge({ years: yearsDiff, months: monthsDiff });
+    setShowAge(true);
+    setDateWarning(false);
+  };
+
+  const displayAge = () => {
+    if (age.years < 1) {
+      const months = age.months === 1 ? 'month' : 'months';
+      return <p>You are {age.months} {months} old!</p>;
+    } else {
+      return (
+        <h4>
+          You are {age.years} years {age.months} months old!
+        </h4>
+      );
+    }
   };
 
   return (
-    <div class="div1">
+    <div className="div1">
       <h1>Age Calculator</h1>
       <h3>Enter your Date of Birth</h3>
+      {dateWarning && (
+        <p className="warning">Please enter a valid date!!!</p>
+      )}
       <input
         type="date"
         value={dob}
         onChange={(e) => setDob(e.target.value)}
         className="inputField"
-      /><br/>
+      />
+      <br />
       <button onClick={calculateAge} className="calculateButton">
         Calculate Age
       </button>
-      {age.years > 0 && (
-        <p>
-          Your are {age.years} years {age.months} months old!!!
-        </p>
-      )}
+      {showAge && <div>{displayAge()}</div>}
     </div>
   );
 };
